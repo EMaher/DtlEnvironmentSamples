df872e0dad5fccecddd35f76c0bc66893e0913d3
Elizabeth Maher (NEWMAN) <enewman@microsoft.com>
2018-06-12 15:07:22 -0700

    adding public ip, fixing script extension

--- VNetWithVM/CustomScripts/ServerConfigScripts.ps1
+++ VNetWithVM/CustomScripts/ServerConfigScripts.ps1
@@ -1,3 +1,9 @@
 <# Custom Script for Windows #>
 
+if ((Test-Path $env:HomeDrive\temp\scriptInfo.txt) -eq $false)
+{
+	New-Item $env:HomeDrive\temp -ItemType Directory -Force
+	New-Item $env:HomeDrive\temp\scriptInfo.txt -ItemType File
+}
+
 Add-Content $env:HomeDrive\temp\scriptInfo.txt "Script ran $(Get-Date)"
